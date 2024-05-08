48b114c7b60de364d4ae182e1476730a0e0c33d4
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-08 14:18:48 +0200

    aggiunta commenti

--- JAVASCRIPT/BANCOMAT/script.js
+++ JAVASCRIPT/BANCOMAT/script.js
@@ -1,12 +1,13 @@
+//Funzione che verifica se le credenziali corrispondono con quelle di un utente salvato
 function verificaCredenziali(nomeUtente, password) {
-    for (let utente of utenti) {
+    for (let utente of utenti) //cicla su tutti gli utenti registrati finché non trova una corrispondenza{
         if (utente.username === nomeUtente && utente.password === password) {
             return true;
         }
     }
     return false;
-}
 
+//Funzione ausiliaria che ritorna un oggetto utente
 function getUtente(username){
     for (let utente of utenti) {
         if (utente.username === username){
@@ -14,7 +15,7 @@ function getUtente(username){
         }
     }
 }
-
+//Funzione che permette l'accesso solo dopo aver controllato le credenizali
 function accedi(){
     let username = prompt("Inserisci il tuo username: ");
     let password = prompt("Inserisci la password: ");
@@ -22,29 +23,31 @@ function accedi(){
     if (verificaCredenziali(username, password)){
         utenteLoggato = getUtente(username);
         alert("Accesso consentito, Benvenuto "+utenteLoggato.username);
-        return operazioni(utenteLoggato);
+        return operazioni(utenteLoggato);//se le credenziali sono corrette accediamo al menu delle operazioni
     }else{
-        return alert("Credenziali errate");
+        return alert("Credenziali errate");//altrimenti stampa messaggio di errore e termina
     }
 }
-
+//Funzione che stampa a schermo le operazioni disponibili e e aspetta la scelta di un operazione da eseguire
 function operazioni(utenteLoggato){
     let menuOperazioni = prompt("Inserisci (1) per bonifico, (2) per deposito (3) per prelievo");
     switch (menuOperazioni) {
         case "1":
-            bonifico(utenteLoggato);
+            bonifico(utenteLoggato);//manda alla funzione bonifico e vuole come parametro l'utente delle quali sono state verificate le credenziali
             break;
         case "2":
-            deposito(utenteLoggato);
+            deposito(utenteLoggato);//manda alla funzione deposito e vuole come parametro l'utente delle quali sono state verificate le credenziali
             break;
         case "3":
-            prelievo(utenteLoggato);
+            prelievo(utenteLoggato);//manda alla funzione prelievo e vuole come parametro l'utente delle quali sono state verificate le credenziali
             break;
         default:
-            alert("Scelta non valida. Riprova.");
+            alert("Scelta non valida. Esco...");//scelta non valida termina.
     }
 }
-   
+//funzione ausiliaria di bonifico la quale accetta un iban ne verifica la  
+//corretteza tra gli utenti registrati e appena ne matcha uno ritorna l'oggetto utente
+//altrimenti invia un messagio di errore e termina
 function getUtenteIban(iban){
     for (let utente of utenti) {
         if (utente.iban=== iban) {
@@ -53,28 +56,29 @@ function getUtenteIban(iban){
     }
     return alert("Iban non corretto");
 }
-
+//funzione che stampa a schermo il saldo dell'utente
 function getSaldo(utente){
     alert("Il tuo saldo, "+ utente.username + " é : "+utente.saldo);
 } 
 
+//Funzione che esegue il bonifico dopo aver effttuato i controlli quali: 
 function bonifico(utenteLoggato){
     let ibanDestinatario = prompt("Inserire iban destinatario");
     let utenteDestinatario = getUtenteIban(ibanDestinatario);
     let importo = prompt("Inserisci importo: ");
-    if(utenteLoggato.username === utenteDestinatario.username){
+    if(utenteLoggato.iban === utenteDestinatario.iban){ //non é possibile effettuare un bonifico a se stessi
         alert("Non puoi fare un bonifico a te stesso");
-    }else if(importo>utenteLoggato.saldo){
+    }else if(importo>utenteLoggato.saldo){ //Non é possibile effettuare un bonifico se non si ha abbastanza saldo
         alert("Importo superiore al saldo");
     }else{
-        importo = parseFloat(importo);
+        importo = parseFloat(importo);//controlli superati effettua il bonifico
         utenteLoggato.saldo -= importo;
         utenteDestinatario.saldo += importo;
     }
     getSaldo(utenteLoggato);
     alert(utenteDestinatario.saldo);
 }
-
+//Funzione che esegue il prelievo di denaro 
 function prelievo(utenteLoggato){
     let importo = prompt("Inserisci l'importo da prelevare: ");
     importo = parseFloat(importo);
