a2a91e11f93f51d47217cb541df9392b08ea2b75
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-09 11:56:17 +0200

    mod

---
+++ JAVASCRIPT/CHECKBOX/script.js
@@ -0,0 +1,17 @@
+//  let checkBox = document.getElementById("box");
+
+//  checkBox.addEventListener("click", function(){
+//     //verifico se la mia checkbox é checkata
+//     if(checkBox.checked){
+//         console.log("Checkbox selezionata");
+//     }else{
+//         console.log("Checkbox non selezionata");
+//     }
+
+//     //ottenere il valore
+//     let valore = checkBox.value;
+
+
+//  })
+
+let listaSpe
