4ad4ff3e0a1fe162b49193eaed2a05cff9a45b35
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-09 12:53:17 +0200

    stile completo

--- JAVASCRIPT/HAMTOTEM/script.js
+++ JAVASCRIPT/HAMTOTEM/script.js
@@ -6,12 +6,14 @@ const coupon = document.getElementById("coupon");
 let scontoContainer = document.getElementById("sconto");
 const finale = document.getElementById("finale");
 const totale = document.getElementById("totale");
+const nome = document.getElementById("nome");
+
 
 //VARIABILI
 let prezzo = 1;
 
+benvenuto();
 
-alert("Ciao, con SIMONE10 hai il 10% di sconto");
 
 checkboxes.forEach(function (checkbox) {
     checkbox.addEventListener("click", function () {
@@ -35,6 +37,8 @@ checkout.addEventListener("click", function(){
     finale.textContent = parseFloat(prezzo) - scontoCalcolato;
 })
 
+
+
 reset.addEventListener("click", function(){
     if(coupon.value != ""){
         coupon.value = "";
@@ -42,11 +46,19 @@ reset.addEventListener("click", function(){
     totale.innerText = "";
     sconto.innerText = "";
     finale.innerText = "";
+    nome.innerText = "";
 
     checkboxes.forEach(function(checkbox){
         if(checkbox.checked){
             checkbox.checked = false;
         }
     })
-
+    benvenuto();
 })
+
+function benvenuto(){
+    const nomeCliente = prompt("Inserisci il tuo Nome");
+    nome.innerText = nomeCliente;
+    alert("Ciao, con SIMONE10 hai il 10% di sconto");
+    
+}
